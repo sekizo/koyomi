9ca6b2a700a13d6b6c55b3b47c2e4b1efaed8d7e
sekizo <sekizoworld@mac.com>
2015-11-20 06:36:36 +0900

    Change initialize sequence
    koyomi/init.rb
      require lib/**/*.rb files

--- lib/koyomi.rb
+++ lib/koyomi.rb
@@ -1,14 +1,6 @@
 # encoding: utf-8
 
 require "koyomi/init"
-require "koyomi/version"
-
-require "koyomi/period"
-require "koyomi/month"
-require "koyomi/week"
-require "koyomi/calendar"
-require "koyomi/helper"
-require "koyomi/error"
 
 module Koyomi
 end
--- lib/koyomi/init.rb
+++ lib/koyomi/init.rb
@@ -6,3 +6,7 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 # Define base module
 module Koyomi
 end
+
+Dir.glob(File.join(lib, "**/*.rb")).each do |f|
+  require f
+end
